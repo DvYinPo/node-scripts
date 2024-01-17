53387e6988e7fd1d820cbc37c5630cd700dd7730
yinpo <347972265@qq.com>
2024-01-17 15:58:12 +0800

    fix: legacy peer deps problem

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
-const terser = require("rollup-plugin-terser").terser;
+const terser = require("@rollup/plugin-terser")
 
 const fs = require('node:fs');
 const path = require('node:path');
